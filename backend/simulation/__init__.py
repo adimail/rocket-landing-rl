2ccd90d401687e5abba72a65afd4701056400b8c
Aditya <adimail2404@gmail.com>
2025-04-20 19:07:52 +0530

    fix(circular imports): backend sim config had a curcular import error

--- backend/simulation/__init__.py
+++ backend/simulation/__init__.py
@@ -1,4 +1,3 @@
 from .controller import SimulationController
-from backend.simulation.config import get_physics_config, get_environment_config
 
-__all__ = ["SimulationController", "get_physics_config", "get_environment_config"]
+__all__ = ["SimulationController"]
